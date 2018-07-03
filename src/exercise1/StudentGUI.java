623b88a67c6a88fc6ac08a9b2ee9f1b5214c74f3
Chien-Hua <39634918+Chien-Hua@users.noreply.github.com>
2018-07-03 14:35:50 -0400

    Initial commit

---
+++ src/exercise1/StudentGUI.java
@@ -0,0 +1,23 @@
+package exercise1;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class StudentGUI extends Frame{
+    private JPanel contentPane;
+    private JTextField textField;
+    private JTextField textField_1;
+    private JTextField textField_2;
+    private JTextField textField_3;
+    private JTextField textField_4;
+    private JTextField textField_5;
+    private JTextField textField_6;
+    private JTextArea textArea;
+    private JComboBox comboBox;
+    private JRadioButton rdbtnNewRadioButton;
+    private JRadioButton rdbtnBusiness;
+    private ButtonGroup radioGroup;
+    private JTextArea textArea_1;
+    private JCheckBox checkBox_1;
+    private JCheckBox chckbxVolunteerWork;
+
+}
